73a1ccdd4f8721c7a5ac6b1ed8e7428844af9dad
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-05-30 23:16:29 +0600

    Creating pages and Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import NavBar from "./components/layouts/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage";
+import FAQPage from "./pages/FAQPage";
+import LoginPage from "./pages/LoginPage";
+import ContactPage from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
-    <>
-      <main className="container xl:container lg:container md:container sm:container mx-auto">
-        <h2>Tailwind CSS Install</h2>
+    <BrowserRouter>
+      <NavBar />
+      <main className="">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layouts/NavBar.jsx
@@ -0,0 +1,33 @@
+import {Link} from 'react-router-dom'
+import logo from "../assets/images/logo-bookmark.svg"
+
+function NavBar() {
+  return (
+    <nav class="container relative mx-auto p-6">
+      {/* Flex Container For Nav Items */}
+      <div class="flex items-center justify-between space-x-20 my-6">
+        {/* Logo */}
+        <Link to="/" class="z-30">
+          <img src={logo} alt="" id="logo" />
+        </Link>
+
+        {/* Menu Items */}
+        <div class="hidden items-center space-x-10 uppercase text-grayishBlue md:flex">
+          <Link to="/" className='tracking-widest hover:text-softRed'>Home</Link>
+          <Link to="/about" className='tracking-widest hover:text-softRed'>About</Link>
+          <Link to="/faq" className='tracking-widest hover:text-softRed'>FAQ</Link>
+          <Link to="/contact" className='tracking-widest hover:text-softRed' >Contact</Link>
+          <Link to="/login" 
+            className='px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md hover:text-softRed hover:bg-white'>
+              Login
+          </Link>
+        </div>
+        {/* @Todo Hamburger Button  */}       
+        
+      </div>
+      {/* @Todo Mobile Menu */}
+    </nav>
+  )
+}
+
+export default NavBar
